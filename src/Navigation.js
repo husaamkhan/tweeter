d2e4a36001be4817094c3de3a8d20d405339819c
Husaam Khan <husaamkhan04@gmail.com>
2024-06-23 12:18:37 -0400

    feat: Created navigation side bar. Have to add to app

---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+const Navigation = () => {
+	return (
+		<div className="navigation">
+			<h1 className="nav-button">Home</h1>
+			<h1 className="nav-button">Search</h1>
+			<h1 className="nav-button">Notifications</h1>
+			<h1 className="nav-button">Profile</h1>
+			<h1 className="post-button">Post</h1>
+		</div>
+	)
+}
+
+export default Navigation
