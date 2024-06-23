d4b66fe5d942bbf67ad995a8f2be8477d79484a2
Husaam Khan <husaamkhan04@gmail.com>
2024-06-23 11:38:01 -0400

    feat: Started project. Designed database

---
+++ db/initDB.sql
@@ -0,0 +1,94 @@
+DROP DATABASE IF EXISTS tweeter_db;
+CREATE DATABASE tweeter_db;
+
+USE tweeter_db;
+
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(45) NOT NULL UNIQUE,
+    email VARCHAR(45) NOT NULL UNIQUE,
+    password VARCHAR(45) NOT NULL,
+    profile_picture_id INT NOT NULL DEFAULT (1),
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    followers INT NOT NULL,
+    following INT NOT NULL,
+    notifications INT NOT NULL,
+    FOREIGN KEY (profile_picture_id) REFERENCES profile_pictures(profile_picture_id)
+);
+
+CREATE TABLE profile_pictures (
+	profile_picture_id INT PRIMARY KEY AUTO_INCREMENT,
+    filepath VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE follows (
+    follower_id INT,
+    followee_id INT,
+    PRIMARY KEY (follower_id , followee_id),
+    FOREIGN KEY (follower_id)
+        REFERENCES users (user_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (followee_id)
+        REFERENCES users (user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE tweets (
+    tweet_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR(200) NOT NULL,
+    images INT NOT NULL,
+    likes INT NOT NULL,
+    replies INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE likes (
+    like_id INT PRIMARY KEY AUTO_INCREMENT,
+    tweet_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (tweet_id)
+        REFERENCES tweets (tweet_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE images (
+    image_id INT PRIMARY KEY AUTO_INCREMENT,
+    tweet_id INT NOT NULL,
+    filepath VARCHAR(45) NOT NULL,
+    FOREIGN KEY (tweet_id)
+        REFERENCES tweets (tweet_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE replies (
+    reply_tweet_id INT NOT NULL,
+    original_tweet_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (reply_tweet_id , original_tweet_id),
+    FOREIGN KEY (reply_tweet_id)
+        REFERENCES tweets (tweet_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (original_tweet_id)
+        REFERENCES tweets (tweet_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
+        ON DELETE CASCADE
+); 
+
+CREATE TABLE notifications (
+	notification_id INT NOT NULL,
+    user_id INT NOT NULL,
+    text VARCHAR(200) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(notification_id, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
