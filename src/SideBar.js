189a15f0bbffde213b8111ec49d5a700fc6aefb3
Husaam Khan <husaamkhan04@gmail.com>
2024-06-23 12:25:47 -0400

    feat: Created sidebar file. Has not been added to app yet

---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+import Navigation from './Navigation';
+
+const Sidebar = () => {
+	return (
+		<div>
+			<h1 className="logo">Tweeter</h1>
+			<Navigation />
+		</div>
+	)
+}
+
+export default Sidebar
