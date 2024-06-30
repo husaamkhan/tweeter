817f179016c867e9d43d96bdb28058e77a9877ab
Husaam Khan <husaamkhan04@gmail.com>
2024-06-30 13:58:17 -0400

    Completed registration page

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -1,5 +1,52 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 const RegistrationPage = () => {
-	const handleCreateAccountClick = () => {};
+	const navigate = useNavigate();
+
+	const [data, setData] = useState({
+		firstname: "",
+		lastname: "",
+		username: "",
+		password: ""
+	});
+
+	const handleRegister = async () => {
+		try{
+			if ( data.firstname == "" || data.lastname == "" || data.username == "" || 
+				data.password == "" ) {
+				alert("Please fill all fields!");
+				return;
+			}
+
+			const response = await axios.post("/user/register", data, {
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+
+			if ( response.status === 200 ) {
+				alert("Registration successful!");
+				navigate("/");
+			}
+
+		} catch (err) {
+			if ( err.response.status === 400 ) {
+				alert("That username is already taken! Please try a different username.");
+			} else {
+				alert("An error occured during registration! Please try again later.");
+			}
+		}
+	};
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setData({
+			...data,
+			[name]: value
+		});
+	};
 
 	return (
 		<div className="auth-page">
@@ -7,23 +54,24 @@ const RegistrationPage = () => {
 				<h1 className="logo">Tweeter</h1>
 				<div className="form-container">
 					<div className="input-container">
-						<label for="firstname-input">First Name:</label>
-						<input type="text" id="firstname-input"/>
+						<label>First Name:</label>
+						<input type="text" name="firstname" onChange={handleChange} />
 					</div>
 					<div className="input-container">
-						<label for="lastname-input">Last Name:</label>
-						<input type="text" id="lastname-input"/>
+						<label>Last Name:</label>
+						<input type="text" name="lastname" onChange={handleChange} />
 					</div>
 					<div className="input-container">
-						<label for="username-input">Username:</label>
-						<input type="text" id="username-input"/>
+						<label>Username:</label>
+						<input type="text" name="username" onChange={handleChange} />
 					</div>
 					<div className="input-container">
-						<label for="password-input">Password:</label>
-						<input type="text" id="password-input"/>
+						<label>Password:</label>
+						<input type="password" className="password-input" name="password"
+							onChange={handleChange} />
 					</div>
 				</div>
-				<button onClick={handleCreateAccountClick}>Create Account</button>
+				<button onClick={handleRegister}>Create Account</button>
 			</div>
 		</div>
 	);
