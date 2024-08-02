564fc981238c66028d965dc51580fa235773f825
Husaam Khan <husaamkhan04@gmail.com>
2024-08-02 15:29:21 -0400

    feat: gets feed from server

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,57 @@
 import Navigation from './Navigation';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const HomePage = () => {
+	const [feed, setFeed] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect( () => {
+		const fetchFeed = async () => {
+			try {
+				const response = await axios.get("/user/feed");
+				setFeed(response.data);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		}
+
+		fetchFeed();
+	}, []);	
+
+	if ( loading ) {
+		return (
+			<div className="home-container">
+				<Navigation />
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
+
+	if ( error ) {
+		return (
+			<div className="home-container">
+				<Navigation />
+				<h1>Oops! An error occured.</h1>
+			</div>
+		);
+	}
+
 	return (
 		<div className="home-container">
 			<Navigation />
-			<h1>Home Page</h1>
+			{feed.length > 0 ? (
+				<ul>
+					{feed.map((post) => (
+						<li key={post.id}>{post.content}</li>
+					))}
+				</ul>
+			) : (
+				<h1>No tweets to see yet!</h1>
+			)}
 		</div>
 	); 
 }
