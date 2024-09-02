9f614e66b2bfc5f488e6bcc4b83d071cdc4be2fc
Husaam Khan <husaamkhan04@gmail.com>
2024-09-02 16:17:15 -0400

    feat: added config.js file for database

---
+++ server/config.js
@@ -0,0 +1,9 @@
+const db_config = {
+	host: 'localhost',
+	port: 3306, // default MySQL port
+	user: 'x', // enter your username here
+	password: 'x', // enter your password here
+	database: 'tweeter_db'
+};
+
+module.exports = db_config;
